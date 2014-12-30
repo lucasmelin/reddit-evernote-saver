6107f4db74eb56e7f01eacf136d8da97bfe8f0fa
Sapping <lucas.melin@gmail.com>
2014-12-30 15:38:11 -0500

    Updated to version 0.2
    URL and comment URL now included. Subreddit added as a tag in Evernote.
    Title of the note now includes username and reddit title.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-Reddit Evernote Saver is a simple script that saves a reddit user's comments as individual notes in Evernote. 
+Reddit Evernote Saver
+=======
+
+This is a script that saves a reddit user's comments as individual notes in Evernote.
+
+The script also saves the URL, reddit comment URL, and adds the subreddit as a tag in Evernote.
+
+Dependencies
+=======
+- [PRAW: The Python Reddit API Wrapper(https://github.com/praw-dev/praw)]
+
+- [Evernote SDK for Python 3(https://github.com/evernote/evernote-sdk-python3)]
+
+TODO
+=======
+- Convert reddit markdown to ENML
+- Allow the option to choose the notebook when adding.
+- More granular comment saving options (keyword, date, subreddit)
--- reddit-evernote-saver.py
+++ reddit-evernote-saver.py
@@ -1,6 +1,4 @@
 import praw
-import hashlib
-import binascii
 import evernote.edam.userstore.constants as UserStoreConstants
 import evernote.edam.type.ttypes as Types
 
@@ -12,33 +10,54 @@ if auth_token == "EVERNOTE_TOKEN":
     print("To get a developer token, visit " \
           "https://sandbox.evernote.com/api/DeveloperToken.action")
     exit(1)
-user_agent = ("Reddit Evernote Saver 0.1 by Sapping"
+user_agent = ("Reddit Evernote Saver 0.2 by Sapping"
                 "github.com/sapping/reddit-evernote-saver")
 r = praw.Reddit(user_agent=user_agent)
-user_name = "REDDIT_USER"
+user_name = "REDDIT_USERNAME"
+if user_name == "REDDIT_USERNAME":
+    print("Please fill in a valid reddit username")
+    exit(1)
 user = r.get_redditor(user_name)
+# Fetches only the 10 most recent comments.
 comment_limit = 10
 gen = user.get_comments(limit=comment_limit)
 reddit_comments = []
 
+# Create a list of all notes and their associated data
 for comment in gen:
-    comment_data=[comment.id, comment.link_title, comment.body, comment.subreddit, comment.link_url]
+    comment_data=[comment.id, comment.link_title, comment.body, comment.subreddit.display_name, comment.link_url, comment.link_id]
     reddit_comments.append(comment_data)
-    print(comment_data)
 
-# Authenticate Evernote server
+# Authenticate Evernote server. Sandbox mode is set to True
 client = EvernoteClient(token=auth_token, sandbox=True)
 
 user_store = client.get_user_store()
 note_store = client.get_note_store()
+attributes = Types.NoteAttributes()
 
-# Create a new note
-note = Types.Note()
-note.title = str(user_name+"'s comment in "+reddit_comments[0][1])
-note.content = '<?xml version="1.0" encoding="UTF-8"?>'
-note.content += '<!DOCTYPE en-note SYSTEM ' \
-                '"http://xml.evernote.com/pub/enml2.dtd">'
-note.content += "<en-note>"
-note.content += reddit_comments[0][2]
-note.content += "</en-note>"
-note_store.createNote(note)
+tags = []
+tags.append(reddit_comments[0][3])
+print(tags)
+
+# Create a new note and publish it to Evernote
+i = 0
+for comments in reddit_comments:
+    tags = []
+    note = Types.Note()
+    attributes.sourceURL = reddit_comments[i][4]
+    note.attributes = attributes
+    note.title = str(user_name+"'s comment in "+reddit_comments[i][1])
+    tags.append(reddit_comments[i][3])
+    note.tagNames = tags
+    note.content = '<?xml version="1.0" encoding="UTF-8"?>'
+    note.content += '<!DOCTYPE en-note SYSTEM ' \
+                    '"http://xml.evernote.com/pub/enml2.dtd">'
+    note.content += "<en-note>"
+    note.content += "<div>"
+    note.content += "<a href="+"\"http://reddit.com/"+reddit_comments[i][5][3:]+"\">"+"Link to reddit comments"+"</a>"
+    note.content += "</div>"
+    note.content += "<p/>"+reddit_comments[i][2]
+    note.content += "</en-note>"
+    print(note)
+    note_store.createNote(note)
+    i += 1
